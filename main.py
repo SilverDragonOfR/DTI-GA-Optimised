32d73f9eec93f3618a1fc1c9601c1b733b4a1807
SilverDragonOfR <ptripathy013@gmail.com>
2023-10-29 20:14:02 +0530

    add: GA

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from datetime import datetime
 from src.edit import make_store_matrix
 from src.predict import predict
 from src.exhaustive_search import exhaustive_search
+from src.genetic import Genetic
 
 # calculate edit distance between 2 strings -----
 if sys.argv[1]=="1":
@@ -57,3 +58,16 @@ if sys.argv[1]=="4":
     en_time = datetime.now()
     print(f"The Exhaustive Search for {n} divisions : optimal icost {optimal_weights[0]}, dcost {optimal_weights[1]}, rcost {optimal_weights[2]}, max_auc {round(max_auc, 5)}")
     print(f'Time taken: {round((en_time-st_time).total_seconds())} s\n')
+
+if sys.argv[1]=="5":
+
+    generations = int(sys.argv[2])
+    st_time = datetime.now()
+
+    print("\nRUNNING THE GENETIC ALGORITHM:")
+    genetic = Genetic(maxGeneration=generations)
+    optimal_weights, max_auc = genetic.run()
+    en_time = datetime.now()
+    print(f"The Genetic Algorithm ran for {generations} generations : optimal icost {optimal_weights[0]}, dcost {optimal_weights[1]}, rcost {optimal_weights[2]}, max_auc {round(max_auc, 5)}")
+    print(f'Time taken: {round((en_time-st_time).total_seconds())} s\n')
+
